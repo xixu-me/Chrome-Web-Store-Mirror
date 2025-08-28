997b8b7df0a0e64e7052b06275e22825a021c86b
Xi Xu <i@xi-xu.me>
2025-08-28 21:18:32 +0800

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,7 +4,7 @@
 
 // API endpoints
 export const DATA_JSON_URL =
-  "https://github.com/xixu-me/Chrome-Web-Store-Lister/releases/latest/download/data.json";
+  "https://xget.xi-xu.me/gh/xixu-me/Chrome-Web-Store-Lister/releases/latest/download/data.json";
 
 // Cache configuration
 export const CACHE_DURATION = 3600 * 1000; // 1 hour in milliseconds
